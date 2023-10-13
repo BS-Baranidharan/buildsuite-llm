b280464882ad0775ae260c22e25790770950ed57
Baranidharan <baranidharanofficial@gmail.com>
2023-10-13 18:11:57 +0530

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,106 @@
-import Image from 'next/image'
+"use client"
+
+import Image from 'next/image';
+import { useState } from 'react';
+import Head from 'next/head';
+import { MdSend, MdMenu, MdClose } from 'react-icons/md';
 
 export default function Home() {
+
+  const [sideNav, showSideNav] = useState(false);
+  const [country, setCountry] = useState("IND");
+  const [query, setQuery] = useState("");
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="overflow-hidden flex h-screen w-screen flex-col items-center bg-gray-50 text-black">
+      <div className={sideNav ? "w-[250px] slide-in absolute right-0 top-0 h-screen bg-white shadow-2xl p-4 hidden flex-col justify-between nav-slide max-sm:flex" : "w-[250px] absolute right-0 top-0 h-screen bg-white shadow-2xl p-4 hidden flex-col justify-between nav-slide2"}>
+        <div>
+          <div className='flex items-center justify-between w-full mb-10'>
+            <Image alt='BuildSuite Logo' src={"/logo.png"} width={40} height={40} />
+            <MdClose onClick={() => showSideNav(false)} className="text-[24px]"></MdClose>
+          </div>
+
+          <p className="text-[#53D878] cursor-pointer my-3">Purchase Credits</p>
+          <p className="cursor-pointer">Sign out</p>
+        </div>
+        <div className="cursor-pointer w-full py-2 text-center rounded-md shadow-sm outline-none border-[1px] border-[#53D878] text-[#53D878] text-[12px] hover:bg-[#53D878] hover:text-white mr-8">
+          3/5 Credits Remaining
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-[1200px] max-xl:w-[1000px] max-lg:w-full max-lg:px-[3vw] mx-0 h-screen flex flex-col justify-start overflow-hidden">
+
+        <nav className="w-full h-[10vh] flex items-center justify-between max-sm:px-4 max-sm:h-[7vh]">
+          <p className="text-[18px] font-bold cursor-pointer text-[#7CAF75]">BuildSuite</p>
+
+          <MdMenu onClick={() => showSideNav(true)} className="max-sm:block text-[28px] text-slate-400 hidden" ></MdMenu>
+
+          <div className="flex items-center max-sm:hidden">
+            <div className="cursor-pointer px-5 py-2 rounded-md shadow-sm outline-none border-[1px] border-[#53D878] text-[#53D878] text-[14px] hover:bg-[#53D878] hover:text-white mr-8">
+              3/5 Credits Remaining
+            </div>
+            <p className="mr-8 text-[#53D878] cursor-pointer">Purchase Credits</p>
+            <p className="cursor-pointer">Sign out</p>
+          </div>
+        </nav>
+
+        <div className="h-[85vh] max-sm:h-[90vh] flex items-center justify-between max-lg:flex-col-reverse">
+
+          <div className="w-[40%] h-full max-lg:h-[10%] max-lg:w-full max-lg:flex-row flex flex-col shadow-md rounded-sm bg-white border-[1px] border-gray-300 p-8 max-lg:p-2">
+
+            <p className=" max-lg:hidden">Select Country</p>
+
+            <div className="flex items-center mb-4 max-lg:hidden">
+              <button className={country == "IND" ? "flex items-center rounded-sm bg-[#53D878] text-white px-6 py-2 my-3 mr-3" : "flex items-center rounded-sm border-[1px] border-gray-400 hover:border-[#53D878] text-[#182139] px-6 py-2 my-3 mr-3"} onClick={() => setCountry("IND")}>
+                <Image alt='BuildSuite Logo' src={"/ind-flag.jpg"} width={25} height={20} />
+                <p className='ml-3'>India</p>
+              </button>
+              <button className={country == "UAE" ? "flex items-center rounded-sm bg-[#53D878] text-white px-6 py-2 my-3 mr-3" : "flex items-center rounded-sm border-[1px] border-gray-400 hover:border-[#53D878] text-[#182139] px-6 py-2 my-3"} onClick={() => setCountry("UAE")}>
+                <Image alt='BuildSuite Logo' src={"/uae-flag.png"} width={25} height={20} />
+                <p className='ml-3'>UAE</p>
+              </button>
+            </div>
+
+            <div className="flex items-center justify-between mb-3 max-lg:hidden">
+              <p>Query</p>
+              <p className="text-[#53D878] cursor-pointer text-[12px]">Generate Sample</p>
+            </div>
+
+            <textarea rows={12} placeholder="Enter your query" className="p-5 max-lg:hidden rounded-sm text-[16px] outline-none border-[1px] border-gray-400 text-black focus:border-gray-500 mb-5" />
+            <button className="max-lg:hidden cursor-pointer px-5 py-2 rounded-sm shadow-md outline-none text-[16px] bg-[#53D878] text-white">
+              Get Quote
+            </button>
+
+            <div className="w-full max-lg:flex hidden">
+              <input type="text" placeholder="Enter your query" className="p-2 w-[80%] max-sm:w-[85%] rounded-sm text-[16px] outline-none border-none text-black focus:border-gray-500" />
+              <button className=" max-sm:hidden w-[20%] px-5 py-2 rounded-sm shadow-md outline-none text-[14px] bg-[#53D878] text-white">
+                Get Quote
+              </button>
+              <button className="hidden max-sm:flex items-center justify-center w-[15%] rounded-sm shadow-md outline-none text-[24px] bg-[#53D878] text-white">
+                <MdSend></MdSend>
+              </button>
+            </div>
+
+
+          </div>
+
+
+          <div className="w-[58%] h-full max-lg:h-[88%] max-lg:w-full flex flex-col shadow-md rounded-sm bg-white border-[1px] border-gray-300 p-8">
+            <p className="mb-4">Tips</p>
+            <div className="px-4 bg-slate-50">
+              <ul className="list-disc p-4 text-[12px]">
+                <li>To get your answer correct, please ask the query in as much as detail as possible.</li>
+              </ul>
+            </div>
+            {/* <input type="text" className="px-5 py-2 outline-none border-[1px] border-gray-200 text-black text-[14px] hover:bg-blue-700 hover:text-white" /> */}
+          </div>
+
+        </div>
+
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
